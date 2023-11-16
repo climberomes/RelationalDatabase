0f5075b91dfce09ecb7b6600836f9484dbe2279e
climberomes <126999678+climberomes@users.noreply.github.com>
2023-11-16 15:45:39 -0600

    Update fortune.sh
    Changes to reflect function creation

--- fortune.sh
+++ fortune.sh
@@ -6,6 +6,16 @@
 
 echo -e "\n~~ Fortune Teller ~~\n"
 
+GET_FORTUNE() {
+  echo "Ask a yes or no question: "
+  read QUESTION
+}
+
+until [[ $QUESTION == test? ]]
+do
+    GET_FORTUNE
+done
+
 N=$((RANDOM % 6))
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 
