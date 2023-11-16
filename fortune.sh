ee13646eda45f357c60098606c000c999a969f04
climberomes <126999678+climberomes@users.noreply.github.com>
2023-11-16 16:04:50 -0600

    Update fortune.sh
    incorporated argument passing to function and conditional check with some regex

--- fortune.sh
+++ fortune.sh
@@ -7,16 +7,24 @@
 echo -e "\n~~ Fortune Teller ~~\n"
 
 GET_FORTUNE() {
-  echo "Ask a yes or no question: "
+  if [[ ! $1 ]]
+  then
+    echo "Ask a yes or no question: "
+  else
+    echo "Try again. Make sure it ends with a question mark:"
+  fi
+  
   read QUESTION
 }
 
-until [[ $QUESTION == test? ]]
+GET_FORTUNE
+
+until [[ $QUESTION =~ \?$ ]]
 do
-    GET_FORTUNE
+    GET_FORTUNE again
 done
 
 N=$((RANDOM % 6))
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 
-echo ${RESPONSES[$N]}
+echo -e "\n"${RESPONSES[$N]}
