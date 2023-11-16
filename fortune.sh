287a680518285153f8d88264bdcf937367b1b914
climberomes <126999678+climberomes@users.noreply.github.com>
2023-11-16 15:07:14 -0600

    Update fortune.sh
    basic random number with array

--- fortune.sh
+++ fortune.sh
@@ -1,1 +1,12 @@
 #fortuneteller script
+
+#!/bin/bash
+
+# Program to tell a persons fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+N=$((RANDOM % 6))
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+
+echo ${RESPONSES[$N]}
