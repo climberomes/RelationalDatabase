061df21ac7cae212cefac62755470ade7958c069
climberomes <126999678+climberomes@users.noreply.github.com>
2023-11-16 12:51:16 -0600

    Update countdown.sh
    argument decremented with for loop

--- countdown.sh
+++ countdown.sh
@@ -5,30 +5,12 @@
 #!/bin/bash
 # Program that counts down to zero from a given argument
 
-#ALL args
-#echo $*
-
-#print first arg
-#echo $1
-
-#if [[ $1 == arg1 ]]
-#then
-#  echo true
-#fi
-
-#if [[ $1 == arg1 ]]
-#if [[ $1 -lt 5 ]]
-#if [[ $1 -le 5 ]]
-#if [[ $1 -gt 0 ]]
-#then
-#  echo true
-#else
-#  echo false
-#fi
-
 if [[ $1 -gt 0 ]]
 then
-  echo true
+  for (( i = $1; i > 0; i-- ))
+  do
+    echo $i
+  done
 else
   echo "Include a positive integer as the first argument."
 fi
